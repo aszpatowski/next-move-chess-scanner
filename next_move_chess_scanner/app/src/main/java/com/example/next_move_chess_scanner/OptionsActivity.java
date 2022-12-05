4b9739e2a7890b8d26bad690e93c6255eb0ad926
aszpatowski <314211@uwr.edu.pl>
2022-12-05 04:59:44 +0100

    start making options

---
+++ next_move_chess_scanner/app/src/main/java/com/example/next_move_chess_scanner/OptionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.next_move_chess_scanner;
+
+
+/*
+What options:
+- UCI - SAN NOTATION
+- Language
+- what color u are (disabled if true below)
+- ask everytime what color i am
+- turn on/turn off automatic detect chessboard
+-
+ */
+public class OptionsActivity {
+}
