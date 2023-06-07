c6b4b8503c43e9615903037e099f325fd34e2f65
aszpatowski <314211@uwr.edu.pl>
2023-06-08 01:32:32 +0200

    first working two inputs

--- create_models_python/one_neural_net_two_inputs.py
+++ create_models_python/one_neural_net_two_inputs.py
@@ -42,7 +42,7 @@ print(conv_base.summary())
 
 image_size_piece = (32, 32)
 image_size_board = (200, 200)
-batch_size = 64
+batch_size = 32
 EPOCHS = 30
 
 datagen_pieces = ImageDataGenerator(
@@ -65,7 +65,6 @@ datagen_boards = ImageDataGenerator(
         )
 
 PATH_TO_DATA_PIECES = "data_one_net\\pieces"
-PATH_TO_DATA_BOARDS = "data_one_net\\boards"
 train_generator_pieces = datagen_pieces.flow_from_directory(
     PATH_TO_DATA_PIECES,
     target_size=image_size_piece,
@@ -87,6 +86,40 @@ validation_generator_pieces = datagen_pieces.flow_from_directory(
     subset='validation'
 )
 
+PATH_TO_DATA_BOARDS = "data_one_net\\boards"
+
+train_generator_boards = datagen_boards.flow_from_directory(
+    PATH_TO_DATA_BOARDS,
+    target_size=image_size_board,
+    class_mode='categorical',
+    color_mode='rgb',
+    seed=1,
+    shuffle=False,
+    batch_size=batch_size,
+    subset='training'
+)
+validation_generator_boards = datagen_boards.flow_from_directory(
+    PATH_TO_DATA_BOARDS,
+    target_size=image_size_board,
+    class_mode='categorical',
+    color_mode='rgb',
+    seed=1,
+    shuffle=False,
+    batch_size=batch_size,
+    subset='validation'
+)
+
+def generator_two_img(gen1, gen2):
+    genX1 = gen1
+    genX2 = gen2
+    while True:
+        X1i = genX1.next()
+        X2i = genX2.next()
+        yield [X1i[0], X2i[0]], X1i[1]
+
+train_generator = generator_two_img(train_generator_pieces, train_generator_boards)
+validation_generator = generator_two_img(validation_generator_pieces, validation_generator_boards)
+
 
 input_piece = tf.keras.Input(shape=(32, 32, 3), name="piece_image")
 
@@ -99,28 +132,30 @@ x_board = layers.Conv2D(32, kernel_size=(3, 3), activation="relu")(input_board)
 x_board = layers.MaxPooling2D(pool_size=(2, 2))(x_board)
 x_board = layers.Conv2D(64, kernel_size=(3, 3), activation="relu")(x_board)
 x_board = layers.MaxPooling2D(pool_size=(2, 2))(x_board)
-x_board = layers.Conv2D(128, kernel_size=(3, 3), activation="relu")(x_board)
+x_board = layers.Conv2D(128, kernel_size=(3, 3), activation="relu", padding="SAME")(x_board)
 x_board = layers.MaxPooling2D(pool_size=(2, 2))(x_board)
 x_board = layers.Flatten()(x_board)
 x_board = layers.Dense(1024, activation='relu')(x_board)
 
+
 concatenated = layers.concatenate([x_piece, x_board])
-x = layers.Dense(1024, activation='softmax')(concatenated)
-x = layers.Dense(512, activation='softmax')(x)
-x = layers.Dense(256, activation='softmax')(x)
+x = layers.Dense(1024, activation='relu')(concatenated)
+x = layers.Dense(512, activation='relu')(x)
 output = layers.Dense(13, activation='softmax')(x)
 
 model = models.Model([input_piece, input_board], output)
 
 print(model.summary())
-sys.exit(0)
+
 print('Liczba wag poddawanych trenowaniu '
       'przed zamrożeniem bazy:', len(model.trainable_weights))
 
-conv_base.trainable = False
+model.layers[7].trainable = False # vgg16
 
 print('Liczba wag poddawanych trenowaniu '
       'po zamrożeniu bazy:', len(model.trainable_weights))
+
+print(model.summary())
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
@@ -135,17 +170,17 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
 
 
 history = model.fit(
-      train_generator_pieces,
+      train_generator,
       steps_per_epoch=train_generator_pieces.samples // batch_size,
       epochs=EPOCHS,
-      validation_data=validation_generator_pieces,
+      validation_data=validation_generator,
       validation_steps=validation_generator_pieces.samples // batch_size,
       verbose=1,
       callbacks=[model_checkpoint_callback])
 
 make_plot(history, f'one_net_two_inputs_epochs_{EPOCHS}_{name_conv_base}.png', False)
 model.load_weights(checkpoint_filepath)
-model.save('models/one_net_two_inputs_{name_conv_base}.h5')
+model.save(f'models/one_net_two_inputs_{name_conv_base}.h5')
 
 
 converter_model = tf.lite.TFLiteConverter.from_keras_model(model)
